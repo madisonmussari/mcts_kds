def49a678bcbaab530c1e0296bfa2c0d01266519
prangel-git <62568827+prangel-git@users.noreply.github.com>
2021-07-08 10:47:34 -0400

    adding tests.

---
+++ test/test_nim_environment.py
@@ -0,0 +1,60 @@
+from nim import Environment
+
+# Write test functions for each of the methods of nim environment
+
+def test_simple_game():
+    # Initial states
+    environment = Environment([2, 1, 3], 1, 3)
+
+    assert environment.state() == (3, 1, [1, 2, 3])
+    assert environment.is_terminal() == False
+    assert environment.turn() == 1
+
+    valid_actions = {(0,1), (1,1), (1,2) ,(2,1), (2,2), (2,3)}
+    environment_valid_actions = {x for x in environment.valid_actions()}
+    assert valid_actions == environment_valid_actions
+
+
+    # First move, take 2 stones from heap 1 (second heap)
+    environment = environment.what_if((1, 2))
+
+    assert environment.state() == (3, 2, [1, 3])
+    assert environment.is_terminal() == False
+    assert environment.turn() == 2
+
+    valid_actions = {(0,1), (1,1), (1,2) ,(1,3)}
+    environment_valid_actions = {x for x in environment.valid_actions()}
+    assert valid_actions == environment_valid_actions
+
+
+    # Checking that invalid moves don't change the environment
+    environment = environment.what_if((0, 2))
+
+    assert environment.state() == (3, 2, [1, 3])
+    assert environment.is_terminal() == False
+    assert environment.turn() == 2
+
+    valid_actions = {(0,1), (1,1), (1,2) ,(1,3)}
+    environment_valid_actions = {x for x in environment.valid_actions()}
+    assert valid_actions == environment_valid_actions
+
+    # Second move, take 1 stone out of heap 0 (first heap)
+    environment = environment.what_if((0, 1))
+    assert environment.state() == (3, 1, [3])
+    assert environment.is_terminal() == True
+    assert environment.turn() == 0
+    assert environment.value(0) == 1
+    assert environment.value(1) == -1
+    assert environment.value(2) == -1
+
+    valid_actions = {}
+    environment_valid_actions = {x for x in environment.valid_actions()}
+    assert valid_actions == environment_valid_actions
+
+
+
+
+
+
+
+
--- tests/test_nim_environment.py
+++
@@ -1,9 +0,0 @@
-import nim
-
-# Write test functions for each of the methods of nim environment
-
-def test_init():
-    assert 1==1
-    pass
-
-
