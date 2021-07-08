08f7635098f36af5903fa698d93182b2d8bdca03
prangel-git <62568827+prangel-git@users.noreply.github.com>
2021-07-08 10:47:05 -0400

    writing tests.

--- nim/environment.py
+++ nim/environment.py
@@ -7,27 +7,31 @@ class Environment:
     def __init__(self,
                  heap=[10, 10, 10],
                  current_player=0,
-                 players=["A", "B"]):
+                 num_players=2):
         self.heap = heap
         self.current_player = current_player
         pass
 
     #  Returns the identifier for the current player.
-    def turn():
+    def turn(self):
         pass
 
     # Returns an iterable of all actions which can be taken from this environment.
-    def valid_actions():
+    def valid_actions(self):
         pass
 
-    # Returns the state which results from taking action.
-    def what_if(action):
+    # Returns the state which results from taking action. Actions are of the form (heap, stones_to_take_out)
+    def what_if(self, action):
         pass
 
     # Returns True if this state is a terminal state.
-    def is_terminal():
+    def is_terminal(self):
         pass
 
     # Returns the value for this environment.  Values can only be calculated for terminal states.
-    def value():
+    def value(self, player):
+        pass
+
+    # Returns current state (num_players, current_player, heap)
+    def state(self):
         pass
