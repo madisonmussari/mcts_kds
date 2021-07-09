2d5764a1c4eec9d6a688fc8e10f773b56463e0e3
prangel-git <62568827+prangel-git@users.noreply.github.com>
2021-07-09 10:28:05 -0400

    Impriving tests.

--- nim/environment.py
+++ nim/environment.py
@@ -1,67 +1,46 @@
 def i_ll_play():
     print("I'll play")
 
+## TODO: Document the functions. 
+## TODO: Type annotations.
 
 class Environment:
-    # Creates a new Nim environment with the current heap allocation and player.
-    def __init__(self,
-                 heap=[10, 10, 10],
-                 current_player=0,
-                 num_players=2,
-                 game_play = True):
-        self.heap = heap
-        self.current_player = current_player
-        pass
+    # creates a new nim environment
+    def __init__(self, heap=[10, 10, 10], current_player=0, num_players=2):
+
+        self.heap = sorted(heap)
+        self.current_player = current_player if current_player < num_players else 0
+        self.num_players = num_players
 
     #  Returns the identifier for the current player.
-    def turn(self,current_player, num_players):
-        if current_player < num_players:
-            current_player += 1
-        else:
-            current_player = 0
-        return current_player
-
+    def turn(self):
+        return self.current_player
 
     # Returns an iterable of all actions which can be taken from this environment.
-    def valid_actions(self, heap):
-        sort_heap = sorted(self.heap)
-        actions = []
-        stone_num = 0
-        for i in len(sort_heap+1):
-            stone_num = sort_heap[i]
-
-
-
-
-        pass
+    def valid_actions(self):
+        return ((0, 0) for x in range(2))
 
     # Returns the state which results from taking action. Actions are of the form (heap, stones_to_take_out)
     def what_if(self, action):
-
-        pass
+        return self
 
     # Returns True if this state is a terminal state.
-    def is_terminal(self,heap,current_player):
-        if all([ i == 0 for i in heap]):
-            self.value(current_player)
-            return True
-        return False
+    def is_terminal(self):
+        return len(self.heap) == 1
 
     # Returns the value for this environment. Values can only be calculated for terminal states.
     def value(self, current_player):
-        result = 0
-        last_player = current_player
-        score = -1
-        return last_player, score
+        score = None
+        if self.is_terminal() and (current_player < self.num_players):
+            score = 1 if self.current_player == current_player else -1
+        return score
 
     # Returns current state (num_players, current_player, heap)
-    def state(self, num_players, current_player, heap):
-        state = [num_players, current_player, heap]
+    def state(self):
+        state = (self.num_players, self.current_player, self.heap)
         return state
+
 
 # main program -  do I need this?
 # while self.game_play == True:
 #   self.turn()
-
-
-
--- test/test_nim_environment.py
+++ test/test_nim_environment.py
@@ -2,6 +2,103 @@ from context import nim
 
 # Write test functions for each of the methods of nim environment
 
+
+# Test state method
+def test_state():
+    environment = nim.Environment([2, 3, 1], 1, 3)
+    expected_environment = (3, 1, [1, 2, 3])
+    assert environment.state() == expected_environment
+
+    environment = nim.Environment([2, 3, 1], 3, 3)
+    expected_environment = (3, 0, [1, 2, 3])
+    assert environment.state() == expected_environment
+
+
+# Checks the is terminal method
+def test_is_terminal():
+    environment = nim.Environment([3], 1, 3)
+    assert environment.is_terminal() == True
+
+    environment = nim.Environment([2, 3, 1], 1, 3)
+    assert environment.is_terminal() == False
+
+
+# Checks value of terminal environments method
+def test_value():
+    environment = nim.Environment([2], 1, 3)
+    assert environment.value(0) == -1
+    assert environment.value(1) == 1
+    assert environment.value(2) == -1
+    assert environment.value(3) == None  # FIXME: Throw error
+
+    environment = nim.Environment([2, 3, 1], 1, 3)
+    assert environment.value(0) == None
+    assert environment.value(1) == None
+    assert environment.value(2) == None
+    assert environment.value(3) == None  # FIXME: Throw error
+
+
+# Tests turn method
+def test_turn():
+    environment = nim.Environment([2, 3, 1], 0, 3)
+    assert environment.turn() == 0
+
+    environment = nim.Environment([2, 3, 1], 1, 3)
+    assert environment.turn() == 1
+
+    environment = nim.Environment([2, 3, 1], 2, 3)
+    assert environment.turn() == 2
+
+
+# Test what_if method
+def test_what_if():
+    environment = nim.Environment([2, 3, 1], 0,
+                                  3)  # current state (3, 0, [1, 2, 3])
+    environment = environment.what_if(
+        (2, 1))  # Takes 1 stone of third heap (index 2)
+    expected_state = (3, 1, [1, 2, 2])
+    assert expected_state == environment.state()
+
+    environment = environment.what_if(
+        (2, 1))  # Takes 1 stone of third heap (index 2)
+    expected_state = (3, 2, [1, 1, 2])
+    assert expected_state == environment.state()
+
+    environment = environment.what_if(
+        (1, 1))  # Takes 1 stone of second heap (index 1)
+    expected_state = (3, 0, [1, 2])
+    assert expected_state == environment.state()
+
+    environment = environment.what_if((1, 3))  # Invalid move
+    expected_state = (3, 0, [1, 2])  # The environment does not change
+    assert expected_state == environment.state()
+
+    environment = environment.what_if(
+        (1, 3))  # Takes 3 stone of second heap (index 1)
+    expected_state = (3, 0, [1,
+                             2])  # Invalid move. Environment does not change.
+    assert expected_state == environment.state()
+
+    environment = environment.what_if(
+        (0, 1))  # Takes 1 stone from first heap (index 0)
+    expected_state = (3, 1, [2])
+    assert expected_state == environment.state()
+
+    environment = environment.what_if(
+        (0, 1))  # The environment is final. Action not allowed
+    expected_state = (3, 1, [2])  # State does not change
+    assert expected_state == environment.state()
+
+
+# Tests valid action  method
+def test_valid_actions(self):
+    environment = nim.Environment([1, 2, 3], 0, 3)
+    expected_valid_actions = {(0, 1), (1, 1), (1, 2), (2, 1), (2, 2), (2, 3)}
+    returned_valid_actions = {valid for valid in environment.valid_actions()}
+    assert expected_valid_actions == returned_valid_actions
+
+
+# Plays a full game of Nim
 def test_simple_game():
     # Initial states
     environment = nim.Environment([2, 1, 3], 1, 3)
@@ -10,10 +107,9 @@ def test_simple_game():
     assert environment.is_terminal() == False
     assert environment.turn() == 1
 
-    valid_actions = {(0,1), (1,1), (1,2) ,(2,1), (2,2), (2,3)}
+    valid_actions = {(0, 1), (1, 1), (1, 2), (2, 1), (2, 2), (2, 3)}
     environment_valid_actions = {x for x in environment.valid_actions()}
     assert valid_actions == environment_valid_actions
-
 
     # First move, take 2 stones from heap 1 (second heap)
     environment = environment.what_if((1, 2))
@@ -22,10 +118,9 @@ def test_simple_game():
     assert environment.is_terminal() == False
     assert environment.turn() == 2
 
-    valid_actions = {(0,1), (1,1), (1,2) ,(1,3)}
+    valid_actions = {(0, 1), (1, 1), (1, 2), (1, 3)}
     environment_valid_actions = {x for x in environment.valid_actions()}
     assert valid_actions == environment_valid_actions
-
 
     # Checking that invalid moves don't change the environment
     environment = environment.what_if((0, 2))
@@ -34,7 +129,7 @@ def test_simple_game():
     assert environment.is_terminal() == False
     assert environment.turn() == 2
 
-    valid_actions = {(0,1), (1,1), (1,2) ,(1,3)}
+    valid_actions = {(0, 1), (1, 1), (1, 2), (1, 3)}
     environment_valid_actions = {x for x in environment.valid_actions()}
     assert valid_actions == environment_valid_actions
 
@@ -50,11 +145,3 @@ def test_simple_game():
     valid_actions = {}
     environment_valid_actions = {x for x in environment.valid_actions()}
     assert valid_actions == environment_valid_actions
-
-
-
-
-
-
-
-
