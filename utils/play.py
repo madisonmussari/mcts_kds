11c5127af81416e60cc302c4df2755a48bc9015e
MadisonM167 <madison@kingsds.network>
2021-07-13 11:19:32 -0400

    commit

--- utils/play.py
+++ utils/play.py
@@ -1,3 +1,17 @@
 
 def play(environment, players):
-    pass
+    moves = []
+    log_moves = ()
+    while not environment.is_terminal():
+        current_player = environment.turn()
+        action = players(current_player).action(environment)
+        environment = environment.what_if(action)
+        moves = [environment, current_player, action]
+        log_moves = log_moves + (moves)
+    return log_moves
+
+play(([2,3,1],0,3), [0,1,2])
+    
+
+
+
