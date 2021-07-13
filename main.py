2580a208525b0378141df821c73b8f6f3c8c7db1
prangel-git <62568827+prangel-git@users.noreply.github.com>
2021-07-13 11:33:32 -0400

    Testing human player for nim.

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from nim import i_ll_play
+from players import HumanPlayer
+from nim import str_to_action
+from nim import Environment
+from utils import play
 
 if __name__ == "__main__":
-    i_ll_play()
+    start_board = Environment([2, 3, 1], 0, 2)
+    player1 = HumanPlayer(str_to_action)
+    player2 = HumanPlayer(str_to_action)
+
+    log = play(start_board, [player1, player2])
+    print(log)
--- utils/play.py
+++ utils/play.py
@@ -1,17 +1,10 @@
 
 def play(environment, players):
-    moves = []
-    log_moves = ()
+    log_moves = []
     while not environment.is_terminal():
         current_player = environment.turn()
-        action = players(current_player).action(environment)
+        action = players[current_player].action(environment)
         environment = environment.what_if(action)
-        moves = [environment, current_player, action]
-        log_moves = log_moves + (moves)
+        moves = [(environment, current_player, action)]
+        log_moves = log_moves + moves
     return log_moves
-
-play(([2,3,1],0,3), [0,1,2])
-    
-
-
-
