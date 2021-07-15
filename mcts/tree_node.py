11b2c0147f780641b9d22d03b38c858385af23fb
prangel-git <62568827+prangel-git@users.noreply.github.com>
2021-07-15 13:16:19 -0400

    changes in tree_node.

--- mcts/tree_node.py
+++ mcts/tree_node.py
@@ -1,8 +1,14 @@
 class TreeNode:
-    def __init__(self, environment):
+    def __init__(self, environment, parent=None):
         '''
         Initializes a node for montecarlo tree search.
         '''
+        self.environment = environment
+        self.num_visits = 0
+        self.num_wins = 0
+        self.children = []
+        self.is_expanded = False
+        self.parent = parent
         pass
 
     def selection(self):
@@ -12,22 +18,22 @@ class TreeNode:
         '''
         pass
 
-    def simulation(self, selection_function):
+    def simulation(self, rollout_strategy):
         '''
         Runs a current environment until a terminal state and returns its value. 
         It selects new environments based on a selection_function. The selection_function 
-        takes an environment and returns an action. A simple selection_function will return a 
+        takes an environment and returns an action. A simple rollout_strategy will return a 
         random action from all possible actions.
         '''
         pass
 
     def expansion(self):
         '''
-        Expands a current_environment until all its nodes have been visited at least once.
+        Populates children
         '''
         pass
 
-    def backpropagation(self, current_environment, value):
+    def backpropagation(self, value):
         '''
         Propagates the value from a node to all of its ancesters. 
         '''
