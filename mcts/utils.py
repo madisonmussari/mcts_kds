efaf7be835eb5caf58bd2d6d7082051967d665ad
prangel-git <62568827+prangel-git@users.noreply.github.com>
2021-07-26 16:00:39 -0400

    adding documentation.

--- mcts/utils.py
+++ mcts/utils.py
@@ -1,2 +1,5 @@
 def random_rollout(environment):
+    '''
+    Returns a new environment based on a random action on the input environment.
+    '''
     return environment.what_if(environment.random_action())
