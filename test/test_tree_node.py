c85252c67f3907d76e1875c7ead29eaf58f05067
prangel-git <62568827+prangel-git@users.noreply.github.com>
2021-08-03 20:17:18 -0400

    Changing TreeNode to keep cache, have several parents, and keep a dictionary for children.

--- test/test_tree_node.py
+++ test/test_tree_node.py
@@ -6,10 +6,13 @@ def make_expanded_node(heap=[3, 2, 4], current_player=0, num_players=2):
     environment = nim.Environment(heap, current_player, num_players)
     parent_node = mcts.TreeNode(environment)
 
-    parent_node.children = [
-        mcts.TreeNode(environment.what_if(action), parent_node, action)
-        for action in environment.valid_actions()
-    ]
+    for action in environment.valid_actions():
+        child_environment = environment.what_if(action)
+        child_node = mcts.TreeNode(child_environment, parent_node.cache)
+        parent_node.children[action] = child_node
+        parent_node.cache[child_environment] = child_node
+
+    parent_node.is_expanded = True
 
     return parent_node
 
@@ -20,14 +23,16 @@ def make_expanded_node(heap=[3, 2, 4], current_player=0, num_players=2):
 def test_selection_1():
     parent_node = make_expanded_node()
 
-    for count, child in enumerate(parent_node.children, 1):
+    actions = []
+    for count, (action, child) in enumerate(parent_node.children.items(), 1):
         child.num_visits = 2 * count
         child.agent_to_value = [count, -count]
         parent_node.num_visits += 2 * count
+        actions.append(action)
 
     # Since all children have the same score, and exploreation parameter is different from zero,
     # we should explore the least visited child.
-    expected_selection = {parent_node.children[0]}
+    expected_selection = {actions[0]}
     selected = set(parent_node.selection())
     assert expected_selection == selected
 
@@ -35,29 +40,33 @@ def test_selection_1():
 def test_selection_2():
     parent_node = make_expanded_node()
 
-    for count, child in enumerate(parent_node.children, 1):
+    actions = []
+    for count, (action, child) in enumerate(parent_node.children.items(), 1):
         child.num_visits = 2 * count
         child.agent_to_value = [-count, count]
         parent_node.num_visits += 2 * count
+        actions.append(action)
 
     # Since all children have the same score, and exploreation parameter is different from zero,
     # we should explore the least visited child.
-    expected_selection = {parent_node.children[0]}
+    expected_selection = {actions[0]}
     selected = set(parent_node.selection())
     assert expected_selection == selected
 
 
 def test_selection_3():
     parent_node = make_expanded_node()
-
-    for count, child in enumerate(parent_node.children, 1):
+    
+    actions = []
+    for count, (action, child) in enumerate(parent_node.children.items(), 1):
         child.num_visits = 2 * count
         child.agent_to_value = [count, -count]
         parent_node.num_visits += 2 * count
+        actions.append(action)
 
     # Since all children have the same score, and exploreation parameter is zero,
     # All children are equally good.
-    expected_selection = set(parent_node.children)
+    expected_selection = set(actions)
     selected = set(parent_node.selection(0))
     assert expected_selection == selected
 
@@ -65,27 +74,31 @@ def test_selection_3():
 def test_selection_4():
     parent_node = make_expanded_node()
 
-    for count, child in enumerate(parent_node.children, 1):
+    actions = []
+    for count, (action, child) in enumerate(parent_node.children.items(), 1):
         child.num_visits = count
         child.agent_to_value = [1, -1]
         parent_node.num_visits += count
+        actions.append(action)
 
     # With explorarion parameter equal to zero, we should select the best score (the first child)
-    expected_selection = {parent_node.children[0]}
+    expected_selection = {actions[0]}
     selected = set(parent_node.selection(0))
     assert expected_selection == selected
 
 
 def test_selection_5():
     parent_node = make_expanded_node(current_player=1)
-
-    for count, child in enumerate(parent_node.children, 1):
+    
+    actions = []
+    for count, (action, child) in enumerate(parent_node.children.items(), 1):
         child.num_visits = count
         child.agent_to_value = [1, -1]
         parent_node.num_visits += count
+        actions.append(action)
 
     # With explorarion parameter equal to zero, we should select the best score (the last child)
-    expected_selection = {parent_node.children[-1]}
+    expected_selection = {actions[-1]}
     selected = set(parent_node.selection(0))
     assert expected_selection == selected
 
@@ -102,14 +115,16 @@ def test_expansion_1():
     assert root_node.is_expanded == False
 
     root_node.expansion()
-    expected_children_after_expansion = {
-        mcts.TreeNode(environment.what_if(action), root_node, action)
-        for action in environment.valid_actions()
-    }
-    assert set(root_node.children) == expected_children_after_expansion
+    
+    expected_expanded_node = make_expanded_node([2, 3, 1], 0, 2)
+
+    for action, child_node in root_node.children.items():
+        assert child_node.environment == expected_expanded_node.children[action]
+
     assert root_node.is_expanded == True
 
 def test_expansion_2():
+    # First example
     environment = nim.Environment([2, 2, 2], 2, 3)
     root_node = mcts.TreeNode(environment)
 
@@ -120,66 +135,27 @@ def test_expansion_2():
     assert root_node.is_expanded == False
 
     root_node.expansion()
-    expected_children_after_expansion = {
-        mcts.TreeNode(environment.what_if(action), root_node, action)
-        for action in environment.valid_actions()
-    }
-    assert set(root_node.children) == expected_children_after_expansion
+    
+    expected_expanded_node = make_expanded_node([2, 2, 2], 2, 3)
+
+    for action, child_node in root_node.children.items():
+        assert child_node.environment == expected_expanded_node.children[action]
+
     assert root_node.is_expanded == True
 
 
 def test_backpropogation():
     environment = nim.Environment([2, 2, 2], 2, 3)
-    node_0 = mcts.TreeNode(environment)
-    node_1 = mcts.TreeNode(environment, parent=node_0)
-    node_2 = mcts.TreeNode(environment, parent=node_1)
-    node_3 = mcts.TreeNode(environment, parent=node_2)
+    node = mcts.TreeNode(environment)
+    node.expansion()
 
-    assert node_0.num_visits == 0
-    assert node_1.num_visits == 0
-    assert node_2.num_visits == 0
-    assert node_3.num_visits == 0
-
-    assert node_0.agent_to_value == [0,0,0]
-    assert node_1.agent_to_value == [0,0,0]
-    assert node_2.agent_to_value == [0,0,0]
-    assert node_3.agent_to_value == [0,0,0]
-
-    node_3.backpropagation([1, 0, -1])
-
-    assert node_0.num_visits == 1
-    assert node_1.num_visits == 1
-    assert node_2.num_visits == 1
-    assert node_3.num_visits == 1
-
-    assert node_0.agent_to_value == [1, 0, -1]
-    assert node_1.agent_to_value == [1, 0, -1]
-    assert node_2.agent_to_value == [1, 0, -1]
-    assert node_3.agent_to_value == [1, 0, -1]
-
-    node_3.backpropagation([-1, 1, 1])
-
-    assert node_0.num_visits == 2
-    assert node_1.num_visits == 2
-    assert node_2.num_visits == 2
-    assert node_3.num_visits == 2
-
-    assert node_0.agent_to_value == [0, 1, 0]
-    assert node_1.agent_to_value == [0, 1, 0]
-    assert node_2.agent_to_value == [0, 1, 0]
-    assert node_3.agent_to_value == [0, 1, 0]
-
-    node_3.backpropagation([-1, 1, 1])
-
-    assert node_0.num_visits == 3
-    assert node_1.num_visits == 3
-    assert node_2.num_visits == 3
-    assert node_3.num_visits == 3
-
-    assert node_0.agent_to_value == [-1, 2, 1]
-    assert node_1.agent_to_value == [-1, 2, 1]
-    assert node_2.agent_to_value == [-1, 2, 1]
-    assert node_3.agent_to_value == [-1, 2, 1]
+    num_visits = 0
+    for _, child_node in node.children.items():
+        child_node.backpropagation([1, 0, -1])
+        num_visits += 1
+    
+    assert node.num_visits == num_visits
+    assert node.agent_to_value == [num_visits, 0, - num_visits]
 
 
 def test_simulation_1():
@@ -201,60 +177,3 @@ def test_simulation_2():
     expected_value = [-1, 1]
     real_value = root_node.simulation()
     assert expected_value == real_value
-
-
-def test_eq():
-    heap = [4,3,1,2]
-    environment_1 = nim.Environment(heap, 0, 2)
-    node_1 = mcts.TreeNode(environment_1)
-
-    environment_2 = nim.Environment(heap, 0, 2)
-    node_2 = mcts.TreeNode(environment_2)
-
-    assert node_1 == node_2
-
-    node_3 = mcts.TreeNode(environment_1, node_2, (3, 3))
-    node_4 = mcts.TreeNode(environment_2, node_1, (3, 3))
-
-    assert node_3 == node_4
-    assert node_1 != node_3
-
-    node_3.num_visits = 10
-    assert node_3 != node_4
-
-    node_4.num_visits = 10
-    assert node_3 == node_4
-
-    node_3.agent_to_value = [-5, 5]
-    assert node_3 != node_4
-
-    node_4.agent_to_value = [-5, 5]
-    assert node_3 == node_4
-
-
-
-def test_hash():
-    heap = [4,3,1,2]
-    environment_1 = nim.Environment(heap, 0, 2)
-    node_1 = mcts.TreeNode(environment_1)
-
-    environment_2 = nim.Environment(heap, 0, 2)
-    node_2 = mcts.TreeNode(environment_2)
-
-    assert hash(node_1) == hash(node_2)
-
-    node_3 = mcts.TreeNode(environment_1, node_2, (3, 3))
-    node_4 = mcts.TreeNode(environment_2, node_1, (3, 3))
-
-    assert hash(node_3) == hash(node_4)
-    assert hash(node_1) != hash(node_3)
-
-    new_heap = [1, 2, 3]
-    environment_5 = nim.Environment(new_heap, 1, 3)
-    node_5 = mcts.TreeNode(environment_5)
-
-    assert hash(node_5) != hash(node_1)
-    assert hash(node_5) != hash(node_3)
-
-# if __name__ == "__main__":
-#     test_simulation_1()
