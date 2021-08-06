f5011d4311793c54cbe94a368962c739906dc2da
MadisonM167 <madison@kingsds.network>
2021-08-06 15:08:40 -0400

    mcts_player

--- players/mcts_player.py
+++ players/mcts_player.py
@@ -1,9 +1,17 @@
+from random import choice
+from mcts.tree_node import TreeNode
 
 class MctsPlayer:
-    def __init__(self, rounds, exploration_param) -> None:
-        
-        pass
+    def __init__(self, cache=dict()):
+        self.cache=cache
 
     def action(self, environment):
+        current_node=self.cache.get(environment, TreeNode(environment, self.cache))
+        if not current_node.is_expanded:
+            current_node.expansion()
         
+        return choice(current_node.selection(0))
+
+
+    def train(self, environment, rounds, exploration_param, oponent=None):
         pass
