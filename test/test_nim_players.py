8a34ab847c5afc9a20a86f9bb8212814740852e1
prangel-git <62568827+prangel-git@users.noreply.github.com>
2021-08-07 09:38:46 -0400

    Correcting tests.

--- test/test_nim_players.py
+++ test/test_nim_players.py
@@ -67,16 +67,15 @@ def test_almost_perfect_player_vs_perfect_player_2():
 
 def test_perfect_player_vs_mcts_player():
     environment = nim.Environment([3, 4, 5], 0, 2)
-    player_0 = nim.PerfectPlayer()
-    player_1 = players.MctsPlayer()
+    player_0 = players.MctsPlayer()
+    player_1 = nim.PerfectPlayer()
 
     log = utils.play(environment, [player_0, player_1])
     (last_environment, _, _) = log[-1]
 
     assert last_environment.is_terminal() == True
-    assert last_environment.value(0) == -1
-    assert last_environment.value(1) == 1
-    pass
+    assert last_environment.value(0) == 1
+    assert last_environment.value(1) == -1
 
 def test_almost_perfect_player_vs_mcts_player():
     environment = nim.Environment([3, 4, 5], 0, 2)
@@ -89,18 +88,17 @@ def test_almost_perfect_player_vs_mcts_player():
     assert last_environment.is_terminal() == True
     assert last_environment.value(0) == -1
     assert last_environment.value(1) == 1
-    pass
 
 def test_random_player_vs_mcts_player():
     environment = nim.Environment([3, 4, 5], 0, 2)
     player_0 = nim.RandomPlayer()
     player_1 = players.MctsPlayer()
+    
+    for _ in range(20):
+        log = utils.play(environment, [player_0, player_1])
+        (last_environment, _, _) = log[-1]
 
-    log = utils.play(environment, [player_0, player_1])
-    (last_environment, _, _) = log[-1]
-
-    assert last_environment.is_terminal() == True
-    assert last_environment.value(0) == -1
-    assert last_environment.value(1) == 1
-    pass
+        assert last_environment.is_terminal() == True
+        assert last_environment.value(0) == -1
+        assert last_environment.value(1) == 1
 
